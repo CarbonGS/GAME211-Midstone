ba18eb516545bda6bf9025212defc0e1b1537460
Stojan Bacvarovski <carbongsocial@gmail.com>
2025-11-26 15:38:54 -0500

    Commit Re-merge branch for audio

---
+++ Audio.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SDL3\SDL.h>
+#include <string>
+
+class Audio {
+
+
+public:
+	struct AudioData {
+		Uint8* pos;
+		Uint32 length;
+	};
+
+	Audio(const std::string& filename);
+
+	~Audio();
+
+	void play();
+	bool isPlaying() const;
+
+private:
+	SDL_AudioSpec wavSpec;
+	Uint8* wavBuffer = nullptr;
+	Uint32 wavLength = 0;
+	SDL_AudioDeviceID dev;
+	SDL_AudioStream* audioStream = nullptr;
+
+	AudioData audioData;
+};
